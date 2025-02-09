e62c9453bc4e2c0fd9b876c0b09ffa63ccca010c
maykon-jds <maykondias2001@gmail.com>
2025-02-09 17:13:15 -0300

    WIP

--- transacao-api/src/main/java/com/desafio/itau/transacao_api/controller/TransacaoController.java
+++ transacao-api/src/main/java/com/desafio/itau/transacao_api/controller/TransacaoController.java
@@ -20,39 +20,36 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/transacao")
 public class TransacaoController {
-	
+
 	private final TransacaoService transacaoService;
-	
+
 	@PostMapping
 	@Operation(description = "Endpoint responsável por adicionar transações")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "201", description = "Transação gravada com sucesso"),
 			@ApiResponse(responseCode = "422", description = "Campos não atendem os requisitos da transação"),
 			@ApiResponse(responseCode = "400", description = "Erro de requisição"),
-			@ApiResponse(responseCode = "500", description = "Erro interno do servidor")
 	})
 	public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoRequestDTO dto){
-		
+
 		transacaoService.adicionarTransacoes(dto);
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).build();
-		
+
 	}
-	
+
 	@DeleteMapping
 	@Operation(description = "Endpoint responsável por deletar transações")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Transação deletadas com sucesso"),
-			@ApiResponse(responseCode = "400", description = "Erro de requisição"),
-			@ApiResponse(responseCode = "500", description = "Erro interno do servidor")
 	})
 	public ResponseEntity<Void> deletarTransacoes(){
-		
+
 		transacaoService.limparTransacoes();
-		
+
 		return ResponseEntity.ok().build();
-		
+
 	}
-	
+
 
 }
